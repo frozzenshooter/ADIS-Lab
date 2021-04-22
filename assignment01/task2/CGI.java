773ff809b30734c7b486113c3f471713127a1451
Nikolas Rank <nikolas.rank@uni-ulm.de>
2021-04-22 12:22:11 +0200

    Mehrere Ordner!

---
+++ assignment01/task2/CGI.java
@@ -0,0 +1,8 @@
+public class CGI {
+
+    public static void main(String[] args) {
+        System.out.println("Content-type: text/html\n\n");
+        System.out.println("Hello, World.");
+    }
+
+}
