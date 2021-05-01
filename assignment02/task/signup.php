7e559d885a5d2d2003470425ee860c2988623929
Steffen Knoblauch <knoblauch.steffen@web.de>
2021-05-01 16:10:03 +0200

    Error handling signup added

--- assignment02/task/signup.php
+++ assignment02/task/signup.php
@@ -1,32 +1,38 @@
 <?php  
 	session_start();   // session starts with the help of this function 
 
+    $error = 0;
 	if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == TRUE)   // Checking whether the session is already there or not if 
 		header("Location: index.php"); 
 
 	if (isset($_POST['signup']))   // it checks whether the user clicked login button or not 
 	{
-		$user = $_POST['loginname'];
-		$pass = $_POST['loginpw'];
-        $pass = password_hash($pass, PASSWORD_DEFAULT);
+        if(isset($_POST['loginname'] && isset($_POST['loginpw'])
+        {
+            $user = $_POST['loginname'];
+            $pass = $_POST['loginpw'];
+            $pass = password_hash($pass, PASSWORD_DEFAULT);
 
-        $db = new SQLite3("roary.db");
-        $db->query('CREATE TABLE IF NOT EXISTS "users"(
-            "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            "username" VARCHAR,
-            "password" VARCHAR
-        )');
+            $db = new SQLite3("roary.db");
+            $db->query('CREATE TABLE IF NOT EXISTS "users"(
+                "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                "username" VARCHAR,
+                "password" VARCHAR
+            )');
 
-        $stmt = $db->prepare("INSERT INTO users ('username', 'password') VALUES (:username, :password)");
-        $stmt->bindValue(":username",$user,SQLITE3_TEXT);
-        $stmt->bindValue(":password",$pass,SQLITE3_TEXT);
-        $result = $stmt->execute();
-        
-        
-        $_SESSION['loggedin'] = TRUE;
-        $_SESSION['username'] = $user;
+            $stmt = $db->prepare("INSERT INTO users ('username', 'password') VALUES (:username, :password)");
+            $stmt->bindValue(":username",$user,SQLITE3_TEXT);
+            $stmt->bindValue(":password",$pass,SQLITE3_TEXT);
+            $result = $stmt->execute();
+            
+            
+            $_SESSION['loggedin'] = TRUE;
+            $_SESSION['username'] = $user;
 
-        header("Location: index.php");
+            header("Location: index.php");
+        }else{
+            $error = 1;
+        }
 	}
 ?>
 <html>
@@ -39,6 +45,11 @@
             <div class="login-container">
                 <h1>Sign up</h1>
                 <form method="POST">
+                    <?php if($error == 1){ ?>
+                        <div class="alert alert-danher" role="alert">
+                            Error: Your message could not be posted. Input invalid.
+                        </div>
+                    <?php } ?>
                     <div class="form-group row">
                         <label for="loginname" class="col-sm-2 col-form-label">Username</label>
                         <div class="col-sm-10">
