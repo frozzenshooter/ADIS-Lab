7a77a81dae89cc8d93f6cfaf9bf629b505833b71
Steffen Knoblauch <knoblauch.steffen@web.de>
2021-05-01 18:04:56 +0200

    Save message added

--- assignment02/task/savepost.php
+++ assignment02/task/savepost.php
@@ -1,26 +1,41 @@
 <?php
 
-$content = array(
-    "datetime" => date_format(new DateTime(), 'Y-m-d H:i:s'),
-    "name" => filter_var($_POST['fname'], FILTER_SANITIZE_STRING),
-    "msg" => filter_var($_POST['fmsg'], FILTER_SANITIZE_STRING),
-);
+    session_start();   
 
-$message = json_encode($content);
+    // Only a logged in user can save a post
+    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == TRUE && isset($_SESSION['username']))
+    {  
+        // Server-side input validation: 128 character limit
+        if (strlen($_POST['fmsg']) <= 128) {
+            if (isset($_POST['fmsg'])) {
 
-$filepath   = "messages.txt"; // [*UWP*] update when published
-$file       = fopen($filepath, "a") or die("Unable to open file 'messages.txt'.");
+                // Create (connect to) SQLite database in file
+                $db = new PDO('sqlite:roary.db');
+                // Set errormode to exceptions
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// Server-side input validation: 128 character limit
-if (strlen($_POST['fmsg']) <= 128 && strlen($_POST['fname']) <= 128) {
-    if (isset($_POST['fname']) && isset($_POST['fmsg'])) {
-        fwrite($file, "\n". $message); // write complete message to file
-        fclose($file);
-        $result = 0; // success
-    } else {
-        $result = 1; // error: fields not filled
+                //To make sure that the table exists - username just the name - no relationship to other table modeled
+                $db->query('CREATE TABLE IF NOT EXISTS "roars"(
+                    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                    "username" VARCHAR, 
+                    "message" VARCHAR,
+                    "created" DATETIME DEFAULT CURRENT_TIMESTAMP
+                )');
+                
+                $message = filter_var($_POST['fmsg'], FILTER_SANITIZE_STRING);
+                $user = $_SESSION['username'];
+
+                $stmt = $db->prepare("INSERT INTO roars ('username', 'message') VALUES (:username, :message)");
+                $stmt->bindValue(":username",$user, SQLITE3_TEXT);
+                $stmt->bindValue(":message",$message, SQLITE3_TEXT);
+                $result = $stmt->execute();
+
+                $result = 0; // success
+            } else {
+                $result = 1; // error: fields not filled
+            }
+        } else {
+            $result = 2; // error: message is too long
+        }
     }
-} else {
-    $result = 2; // error: message is too long
-}
 ?>
