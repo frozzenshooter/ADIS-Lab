69b55e5ec3ed4098dc3f1658ddc3da6bcf6f39bf
Steffen Knoblauch <knoblauch.steffen@web.de>
2021-05-13 17:19:30 +0200

    Fetching of roarys and the rendering added

--- assignment03/Roary/roaryapp/views.py
+++ assignment03/Roary/roaryapp/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import JsonResponse
-from.models import Roar, Like
+from .models import Roar, Like
 from django.db.models import Count, F
 
 def index(request):
@@ -10,5 +9,8 @@ def index(request):
 def roars(request):
     # Load all roars form the db and count the current likes and resolve the author name
     data = list(Roar.objects.all().annotate(likes=Count('like')).annotate(username = F('author__username')).values())
+    
+    #todo: annotated if the current user has liked the roar
+
     return JsonResponse(data, safe=False)
 
