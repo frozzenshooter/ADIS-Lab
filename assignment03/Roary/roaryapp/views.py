dfa943cb2e5174a6f06d59c3abf143a36220b017
Steffen Knoblauch <knoblauch.steffen@web.de>
2021-05-13 18:39:06 +0200

    Like button added and like state will be returned with roars

--- assignment03/Roary/roaryapp/views.py
+++ assignment03/Roary/roaryapp/views.py
@@ -2,15 +2,26 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Roar, Like
 from django.db.models import Count, F
+from django.db.models import Exists, OuterRef
+from django.utils.datastructures import MultiValueDictKeyError
 
 def index(request):
     return render(request, 'index.html')
 
 def roars(request):
-    # Load all roars form the db and count the current likes and resolve the author name
-    data = list(Roar.objects.all().annotate(likes=Count('like')).annotate(username = F('author__username')).values())
+
+    # Get id fomr an authenticated user
+    current_user_id = -1
+
+    if request.user.is_authenticated:
+        current_user_id = request.user.id        
+
+    # Subquery which will be used to check if a like from this user exists
+    hasUserLiked = Like.objects.filter(roar=OuterRef('pk'), user=current_user_id)
     
-    #todo: annotated if the current user has liked the roar
+    # Load all roars form the db and count the current likes and resolve the author name
+    roars = Roar.objects.all().annotate(likes=Count('like')).annotate(username = F('author__username')).annotate(userHasLiked=Exists(hasUserLiked))
+    data = list(roars.values())
 
     return JsonResponse(data, safe=False)
 
