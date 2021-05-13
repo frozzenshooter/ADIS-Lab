d6052e274e8597cd3493a3ba827a1b1b35073897
Steffen Knoblauch <knoblauch.steffen@web.de>
2021-05-13 17:01:51 +0200

    Json interface for the roars added

--- assignment03/Roary/roaryapp/models.py
+++ assignment03/Roary/roaryapp/models.py
@@ -1,15 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
 
+def convertDatetimeToString(o):
+    DATE_FORMAT = "%Y-%m-%d" 
+    TIME_FORMAT = "%H:%M:%S"
+
+    if isinstance(o, datetime.date):
+        return o.strftime(DATE_FORMAT)
+    elif isinstance(o, datetime.time):
+        return o.strftime(TIME_FORMAT)
+    elif isinstance(o, datetime.datetime):
+        return o.strftime("%s %s" % (DATE_FORMAT, TIME_FORMAT))
+    else :
+        return ""
+
 class Roar(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField()
+    date = models.DateTimeField(auto_now_add=True)
     post = models.TextField(max_length=128)
     
     def __str__(self):
-        return self.author.username+"["+self.date.strftime("%m/%d/%Y, %H:%M:%S")+"]: "+self.post
+        return self.author.username+"["+convertDatetimeToString(self.date)+"]: "+self.post
 
 class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- assignment03/Roary/roaryapp/views.py
+++ assignment03/Roary/roaryapp/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from.models import Roar, Like
+from django.db.models import Count, F
 
 def index(request):
-
     return render(request, 'index.html')
+
+def roars(request):
+    # Load all roars form the db and count the current likes and resolve the author name
+    data = list(Roar.objects.all().annotate(likes=Count('like')).annotate(username = F('author__username')).values())
+    return JsonResponse(data, safe=False)
+
