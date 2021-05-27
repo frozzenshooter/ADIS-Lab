374459c67567e5f553bfa01f37931a281f63177a
Steffen Knoblauch <knoblauch.steffen@web.de>
2021-05-27 10:33:22 +0200

    Static serving

--- assignment04/app.js
+++ assignment04/app.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+app.use('/', express.static('public'));
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 })
+
+// Last route to handle 404
+app.get('*', function(req, res){
+  res.status(404).send('$04- Page not found');
+});
