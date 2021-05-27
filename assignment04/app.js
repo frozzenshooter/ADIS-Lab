adc446373d89e9104be5a321b2bcbcde001f34ca
Steffen Knoblauch <knoblauch.steffen@web.de>
2021-05-27 17:14:49 +0200

    Fixes for fetching introduced

--- assignment04/app.js
+++ assignment04/app.js
@@ -14,52 +14,61 @@ app.use(session({
     saveUninitialized: true,
     cookie: { secure: true }
 }));
-app.post("/login", jsonParser, (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
 
-  //TODO: Check if Login Info is correct
-  const success = true;
-  //TODO: Fetch Username from Database
-  if(success) {
-    const username = "Dummy Name";
-    res.status(200).json(username).send();
-  }else {
-    res.status(403).send();
-  }
+app.post("/login", jsonParser, (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    console.log("Email:", email, "PW:", password);
+
+    //TODO: Check if Login Info is correct
+    const success = true;
+    //TODO: Fetch Username from Database
+    if(success) {
+      const username = "Dummy Name";
+      res.status(200).json(username).send();
+    }else {
+      res.status(403).send();
+    }
 })
 
 app.post("/signup", jsonParser, (req, res) =>{
-  const email = req.body.email;
-  const username = req.body.username;
-  const password = req.body.password;
-  //TODO: Sign the User up, send error if username already taken
-  const error = false;
-  res.status(200).json(error).send();
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
+
+    console.log("Email:", email, "password", password, "username", username);
+
+    //TODO: Sign the User up, send error if username already taken
+    const error = false;
+    res.status(200).json(error).send();
 })
 
 app.post("/logout", jsonParser, (req, res) => {
-  const username = req.body.username;
-  //TODO Sign this User Out
-  res.status(200).send();
+    const username = req.body.username;
+    console.log("username", username);
+    //TODO Sign this User Out
+    res.status(200).send();
 })
 
 app.post("/postRoar", jsonParser, (req, res) =>  {
-  const username = req.body.username;
-  const message = req.body.message;
-  //TODO Save Roar
+    const username = req.body.username;
+    const message = req.body.message;
+    console.log("username", username, "message", message);
+    //TODO Save Roar
+    res.status(200).send();
 })
 
 app.get("/roars", jsonParser, (req, res) => {
-  const username = req.body.username;
-  //TODO Fetch Roars from DB and send
+    //TODO Fetch Roars from DB and send
+    res.status(200).send();
 })
 
 // Last route to handle 404
 app.get('*', function(req, res){
-  res.status(404).send('404- Page not found');
+    res.status(404).send('Error 404 - Page not found');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`);
 });
