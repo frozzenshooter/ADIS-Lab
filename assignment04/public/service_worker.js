c13f9adedd848c2a7e8be617a526af9b8bfed26e
uulm-LG <lukas.gruler@uni-ulm.de>
2021-06-07 13:17:54 +0200

    trying to get the service worker to work but it does not (ultra buggy); bootstrap as file for offline caching

--- assignment04/public/service_worker.js
+++ assignment04/public/service_worker.js
@@ -1,11 +1,46 @@
-var CACHE_NAME = "v1";
+var CACHE_NAME = 'v1';
+
+
+/* MY BUGGY CODE - Lukas */
+
+// var urlsToCache = [
+//   '/index.html',
+//   '/roars',
+//   '/bootstrap.min.css'
+// ];
+
+// self.addEventListener('install', function(event) {
+//   // Perform install steps
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//         .then(function(cache) {
+//             console.log('Opened cache');
+//             return cache.addAll(urlsToCache);
+//         })
+//   );
+// });
+
+// self.addEventListener('fetch', function(event) {
+//     event.respondWith(
+//         caches.match(event.request)
+//             .then(function(response) {
+//             // Cache hit - return response
+//             if (response) {
+//                 return response;
+//             }
+//             return fetch(event.request);
+//             }
+//         )
+//     );
+// });
 
 
 function precache() {
     return caches.open(CACHE_NAME).then(function (cache) {
         return cache.addAll([
         './index.html',
-        './roars'
+        './roars',
+        '/bootstrap.min.css'
         ]);
     });
 }
